c6045e31a383fbfeffc7bfcdca7ed2c799c89931
bryan404 <bryan.lopez.a@mail.pucv.cl>
2022-03-23 14:44:33 -0300

    Se creo la clase ramo

--- Caja.java
+++ Caja.java
@@ -2,6 +2,15 @@ import java.io.*;
 
 public class Caja {
     public static void main(String args[]) throws IOException{
+
+        String datoIngresado;
+
         Alumno[] aux = new Alumno[2];
+        Ramo[] aux2 = new Ramo[2];
+
+        BufferReader x = new BufferReader(new InputStreamReader(System.in));
+
+        System.out.println("Ingrese la cantidad de alumnos ");
+
     }  
 }
---
+++ Ramo.java
@@ -0,0 +1,53 @@
+public class Ramo{
+    String nombreRamo;
+    String codigoRamo;
+    String nombreProfesor;
+    int creditosOtorga;
+    int cantidadAlumnos;
+
+    public Ramo() {
+
+    }
+
+    public Ramo(String nombreRamo, String codigoRamo, String nombreProfesor, int creditosOtorga, int cantidadAlumnos) {
+        this.nombreRamo = nombreRamo;
+        this.codigoRamo = codigoRamo;
+        this.nombreProfesor = nombreProfesor;
+        this.creditosOtorga = creditosOtorga;
+        this.cantidadAlumnos = cantidadAlumnos;
+    }
+
+    public void setNombreRamo(String nombreRamo){
+        this.nombreRamo = nombreRamo;
+    }
+    public void setCodigoRamo(String codigoRamo){
+        this.codigoRamo = codigoRamo;
+    }
+    public void setNombreProfesor(String nombreProfesor){
+        this.nombreProfesor = nombreProfesor;
+    }
+    public void setCreditosOtorga(int creditosOtorga){
+        this.creditosOtorga = creditosOtorga;
+    }
+    public void setCantidadAlumnos(int cantidadAlumnos){
+        this.cantidadAlumnos = cantidadAlumnos;
+    }
+
+    public String getNombreRamo(){
+        return this.nombreRamo;
+    }
+    public String getcodigoRamo(){
+        return this.codigoRamo;
+    }
+    public String getNombreProfesor(){
+        return this.nombreProfesor;
+    }
+    public int getCreditosOtorga(){
+        return this.creditosOtorga;
+    }
+    public int getCantidadAlumnos(){
+        return this.cantidadAlumnos;
+    }
+
+
+}
