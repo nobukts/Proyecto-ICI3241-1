87a82807222736b0ba8ecd6cdc930cefb14d24aa
José Osega <soujiok@gmail.com>
2022-03-25 16:25:20 -0300

    Intento de implementacion en HashMap
    Los println estan desincronizados

--- src/main/java/com/mycompany/avancecurricular/Alumno.java
+++ src/main/java/com/mycompany/avancecurricular/Alumno.java
@@ -1,63 +1,64 @@
 package com.mycompany.avancecurricular;
+import java.util.ArrayList;
 
 public class Alumno {
     private String nombreAlumno;
-    private String[] mallaCurricular;
-    private String[] ramosAprobados;
-    private String[] ramosActuales;
-    private String[] ramosFaltantes;
+    private ArrayList<String> mallaCurricular;
+    private ArrayList<String> ramosAprobados;
+    private ArrayList<String>  ramosActuales;
+    private ArrayList<String> ramosFaltantes;
     private int cantCreditos;
 
     public Alumno(){
         
     }
-    
+
     public String getNombreAlumno() {
         return nombreAlumno;
-    }
-
-    public String[] getMallaCurricular() {
-        return mallaCurricular;
-    }
-
-    public String[] getRamosAprobados() {
-        return ramosAprobados;
-    }
-
-    public String[] getRamosActuales() {
-        return ramosActuales;
-    }
-
-    public String[] getRamosFaltantes() {
-        return ramosFaltantes;
-    }
-
-    public int getCantCreditos() {
-        return cantCreditos;
     }
 
     public void setNombreAlumno(String nombreAlumno) {
         this.nombreAlumno = nombreAlumno;
     }
 
-    public void setMallaCurricular(String[] mallaCurricular) {
+    public ArrayList<String> getMallaCurricular() {
+        return mallaCurricular;
+    }
+
+    public void setMallaCurricular(ArrayList<String> mallaCurricular) {
         this.mallaCurricular = mallaCurricular;
     }
 
-    public void setRamosAprobados(String[] ramosAprobados) {
+    public ArrayList<String> getRamosAprobados() {
+        return ramosAprobados;
+    }
+
+    public void setRamosAprobados(ArrayList<String> ramosAprobados) {
         this.ramosAprobados = ramosAprobados;
     }
 
-    public void setRamosActuales(String[] ramosActuales) {
+    public ArrayList<String> getRamosActuales() {
+        return ramosActuales;
+    }
+
+    public void setRamosActuales(ArrayList<String> ramosActuales) {
         this.ramosActuales = ramosActuales;
     }
 
-    public void setRamosFaltantes(String[] ramosFaltantes) {
+    public ArrayList<String> getRamosFaltantes() {
+        return ramosFaltantes;
+    }
+
+    public void setRamosFaltantes(ArrayList<String> ramosFaltantes) {
         this.ramosFaltantes = ramosFaltantes;
+    }
+
+    public int getCantCreditos() {
+        return cantCreditos;
     }
 
     public void setCantCreditos(int cantCreditos) {
         this.cantCreditos = cantCreditos;
     }
-
+    
 }
--- src/main/java/com/mycompany/avancecurricular/Main.java
+++ src/main/java/com/mycompany/avancecurricular/Main.java
@@ -1,42 +1,26 @@
 package com.mycompany.avancecurricular;
 import java.io.* ;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        HashMap<String, Alumno> mapaAlumnos = new HashMap<>();
+        Alumno alumnoActual = new Alumno();
+        
         BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
-        
-        //Vamos a inicializar las variables
-        int tamañoArreglo;
+        for (int i = 0; i < 10; i++) {
+            String palabraIngresada = lector.readLine();
+            System.out.println("Ingresar el nombre del alumno");
+            alumnoActual.setNombreAlumno(palabraIngresada);
 
-        System.out.println("Ingrese la cantidad de alumnos:");
-        tamañoArreglo = Integer.parseInt(lector.readLine());
-        Alumno[] listaAlumnos = new Alumno[tamañoArreglo];
-
-        //De momento los ramos seran siempre los mismos
-        String[] malla = {"ICI3241" , "ICI3243" , "EST3107"};
-
-        //Se ingresa el nombre del alumno y una malla base
-        for(int i=0 ; i < listaAlumnos.length; i++){
-            System.out.println("Ingrese el alumno " + (i+1) + "/" + listaAlumnos.length + ":");
-            listaAlumnos[i] = new Alumno();
-            listaAlumnos[i].setNombreAlumno(lector.readLine());
-            listaAlumnos[i].setMallaCurricular(malla);
-        }
-        
-        //Se muestra la informacion que se tiene de momento
-        for(int i=0 ; i < listaAlumnos.length; i++){
-            System.out.print("El alumno " + (i+1) + "/" + listaAlumnos.length + ": ");
-            System.out.println(listaAlumnos[i].getNombreAlumno());
-            System.out.print("Su malla es: ");
+            if(palabraIngresada.equals("No")) break;
             
-            for(int k=0 ; k < 3; k++){
-                String[] mallaAux = new String[3];
-                mallaAux = listaAlumnos[i].getMallaCurricular();
-                System.out.println(mallaAux[k]);
-            }
+            System.out.println("Ingresar la cantidad de creditos");
+            alumnoActual.setCantCreditos(Integer.parseInt(lector.readLine()));
 
+            mapaAlumnos.put(alumnoActual.getNombreAlumno(), alumnoActual);   
         }
-
-    }
-    
+        
+    }    
 }
