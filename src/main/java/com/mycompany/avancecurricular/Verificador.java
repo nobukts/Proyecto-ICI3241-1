34d9281c2eae7ef446ac744df18a0567e9d6a757
Nobukts <soujiok@gmail.com>
2022-05-30 11:08:39 -0400

    Correcion de archivos

--- src/main/java/com/mycompany/avancecurricular/Verificador.java
+++
@@ -1,7 +0,0 @@
-package com.mycompany.avancecurricular;
-
-public interface Verificador {
-    //Metodo para verificar informacion en las clases que se implementa
-    public boolean verificar(String nombre);
-    
-}
--- src/main/java/src/main/java/Interfaces/Verificador.java
+++
@@ -1,6 +0,0 @@
-package Interfaces;
-
-public interface Verificador {
-    //Metodo para verificar informacion en las clases que se implementa
-    public boolean verificar(String nombre);  
-}
